372b655a45ccdb281d4126316ef9c613ffc2799d
LimbergMG <limbergmarttinez7105@gmail.com>
2024-04-16 12:07:27 -0500

    login controller y diseño terminado

--- src/main/java/pe/edu/cibertec/warformulariosspring/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/warformulariosspring/controller/LoginController.java
@@ -3,6 +3,9 @@ package pe.edu.cibertec.warformulariosspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import pe.edu.cibertec.warformulariosspring.model.LoginModel;
 
 @Controller
@@ -13,5 +16,16 @@ public class LoginController {
     public String login(Model model){
         model.addAttribute("loginModel", new LoginModel());
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String validarLogin(@ModelAttribute("loginModel")
+                                   LoginModel loginModel, Model model){
+        if(loginModel.getUsuario().equals("lsalvatierra") &&
+                loginModel.getPassword().equals("123")){
+            return"home";
+        }
+        return "login";
+
     }
 }
