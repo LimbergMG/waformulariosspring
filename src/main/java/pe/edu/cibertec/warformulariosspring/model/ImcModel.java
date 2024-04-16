675ad9438c457c97011027024a7bdfefcdfea249
LimbergMG <limbergmarttinez7105@gmail.com>
2024-04-16 12:48:33 -0500

    imc arreglado

---
+++ src/main/java/pe/edu/cibertec/warformulariosspring/model/ImcModel.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.warformulariosspring.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImcModel {
+
+
+    private double talla;
+    private double peso;
+}
